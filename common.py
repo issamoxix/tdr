c68f01f42629ae23dabc1eab754459f483bb4eb4
Issam Laptop <Issam.Haidaoui@hqrevenue.com>
2021-12-12 22:36:27 +0100

    refactor the code

---
+++ common.py
@@ -0,0 +1,39 @@
+import os
+import pathlib
+from bs4 import BeautifulSoup
+import requests
+
+
+def pdf_dwl(self, filename, pdfname, link):
+    link = link
+    fol = str(pathlib.Path().absolute())+'/'+self.endingPath+'/'
+    folder_location = fol+filename
+    filename = os.path.join(folder_location, str(pdfname)+'.pdf')
+    with open(filename, 'wb') as f:
+        f.write(requests.get(link).content)
+    print(filename, 'Done ############################')
+
+
+def get_pdf_link(self, doc, pdf_1, pdf_2):
+    # pdf_n = f'\{pdf_1}\{pdf_2}' #uncomment for window
+    pdf_n = '/'+pdf_1+'/'+pdf_2
+    course_link = []
+    pdf_links = []
+    for list_ in doc.find_all('li', "element"):
+        link = list_.a
+        try:
+            if str(link.span.get('class')[1]) == self.pdf_class:
+                res2 = requests.get(link.get('href'))
+                doc2 = BeautifulSoup(res2.text, "html.parser")
+                div_a = doc2.find('div', [self.div_pdf_link])
+                pdf_link = div_a.a.get("href")
+
+                pdf_dwl(pdf_n, str(
+                    doc2.find('title').get_text()), pdf_link)
+                # add_to_file(pdf_link,'./'+str(pdf_1)+'/'+str(pdf_2))
+                # sys.exit() #debugg
+                # course_link.append(link.get('href'))
+
+        except:
+            # print('Not a pdf link')
+            error = 'Not a pdf link'
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from app import app
 from app2 import app1
 import sys
-def boot():
+
+
+def main():
     print('1: High School \n2: College\n3: Primaire\n0: Exit')
     x = int(input(''))
-    if x==1:
+    if x == 1:
         scraping = app()
     elif x == 2:
         scraping = app1(1)
     elif x == 3:
         scraping = app1(0)
-    elif x ==0:
+    elif x == 0:
         print('Bye !')
         sys.exit()
     else:
-        boot()
-boot()
+        main()
+
+
+if __name__ == "__main__":
+    main()
